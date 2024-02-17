9fe0c5369b9df184ee59b9c4dccefa7d05457d31
Ryota Nishida <24.ryota.sub@gmail.com>
2024-02-17 17:36:25 +0900

    TodoのDB格納処理の修正

--- resources/js/Components/SubmitTodo.jsx
+++ resources/js/Components/SubmitTodo.jsx
@@ -4,6 +4,12 @@ import InputError from '@/Components/InputError'
 import PrimaryButton from '@/Components/PrimaryButton'
 
 export default function SubmitTodo() {
+  // data: 状態を保管。初期値はuseFotm({title: ''})のように与えることができる
+  // setDate: dataを変更するための関数。非同期なので注意が必要。
+  // post: postする関数。他にget, put, patch, deleteがある
+  // processing: フォームの処理が始まるとtrueに変わる。フォームの処理中に送信ボタンを無効にして複数回の送信を防ぐ
+  // reset: dataを初期値に戻す。
+  // errors: Controller側でバリデーションが出た場合、フォールドとエラーメッセージがerrorsに返ってくる。ここではInputErrorコンポーネントに渡してエラーの場合のみメッセージを赤字で表示。
   const {data, setData, post, processing, reset, errors} = useForm({
     'title': '',
   })
